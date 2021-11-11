70e47c69116bf8797bb29aec1d16f12e5c79cc45
sedayegin <86283908+sedayeginn@users.noreply.github.com>
2021-11-11 03:17:26 +0300

    Data katmanı oluşturuldu.

---
+++ BlogApp.Data/Concrete/EntityFramework/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BlogApp.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Data.Concrete.EntityFramework.Contexts
+{
+   public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=Blog-App;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
