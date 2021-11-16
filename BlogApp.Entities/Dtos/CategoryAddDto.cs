f91f2bf8c2afe9468065d0879e848c16598d6ce2
sedayegin <86283908+sedayeginn@users.noreply.github.com>
2021-11-16 15:37:43 +0300

    category manager oluşturuldu.

---
+++ BlogApp.Entities/Dtos/CategoryAddDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Entities.Dtos
+{
+   public class CategoryAddDto
+    {
+        [DisplayName("Kategori Adı : ")]//ne yazmasını istiyorsak onu yazıyoruz
+        [Required(ErrorMessage ="{0} boş geçilemez")]
+        [MaxLength(70, ErrorMessage ="{0} en fazla {1} karakter olabilir")]   //dinamik yaptık
+        [MinLength(3, ErrorMessage ="{0} en az {1} karkter olmalıdır")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Açıklaması : ")]
+        [MaxLength(500, ErrorMessage = "{0} en fazla {1} karakter olabilir")] 
+        [MinLength(3, ErrorMessage = "{0} en az {1} karkter olmalıdır")]
+        public string Description { get; set; }
+        [DisplayName("Kategori Özel Not Alanı : ")]
+        [MaxLength(500, ErrorMessage = "{0} en fazla {1} karakter olabilir")]
+        [MinLength(3, ErrorMessage = "{0} en az {1} karkter olmalıdır")]
+        public string Note { get; set; }
+        [DisplayName("Aktif Mi? : ")]//ne yazmasını istiyorsak onu yazıyoruz
+        [Required(ErrorMessage = "{0} boş geçilemez")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ BlogApp.Entities/Dtos/CategoryUpdateDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Entities.Dtos
+{
+   public class CategoryUpdateDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [DisplayName("Kategori Adı : ")]//ne yazmasını istiyorsak onu yazıyoruz
+        [Required(ErrorMessage = "{0} boş geçilemez")]
+        [MaxLength(70, ErrorMessage = "{0} en fazla {1} karakter olabilir")]   //dinamik yaptık
+        [MinLength(3, ErrorMessage = "{0} en az {1} karkter olmalıdır")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Açıklaması : ")]
+        [MaxLength(500, ErrorMessage = "{0} en fazla {1} karakter olabilir")]
+        [MinLength(3, ErrorMessage = "{0} en az {1} karkter olmalıdır")]
+        public string Description { get; set; }
+        [DisplayName("Kategori Özel Not Alanı : ")]
+        [MaxLength(500, ErrorMessage = "{0} en fazla {1} karakter olabilir")]
+        [MinLength(3, ErrorMessage = "{0} en az {1} karkter olmalıdır")]
+        public string Note { get; set; }
+        [DisplayName("Aktif Mi? : ")]//ne yazmasını istiyorsak onu yazıyoruz
+        [Required(ErrorMessage = "{0} boş geçilemez")]
+        public bool IsActive { get; set; }
+        [DisplayName("Silindi Mi? : ")]//ne yazmasını istiyorsak onu yazıyoruz
+        [Required(ErrorMessage = "{0} boş geçilemez")]
+        public bool IsDeleted { get; set; }
+    }
+}
